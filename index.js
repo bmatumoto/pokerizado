897f18b2d76790d5f0b5fbe29a7bd8a7be37c720
Bruno Matumoto <brunoxd.matumoto@gmail.com>
2022-05-05 19:56:58 -0300

    Fix eslint

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.status(200).send(`Project Pokerizado rodando na porta: ${port}`);
-})
+});
 
 app.get('/pokemon', (_req, res) => {
   res.status(200).json({ port });
-})
+});
 
 app.listen(port, () => {
   console.log(`Aplicação rodando na porta: ${port}`);
-})
+});
