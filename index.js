628b5c622a4385aa4c318312b18f1646565dcf3c
Bruno Matumoto <brunoxd.matumoto@gmail.com>
2022-05-05 18:50:35 -0300

    Add github workflow

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get('/', (req, res) => {
   res.status(200).send(`Project Pokerizado rodando na porta: ${port}`);
 })
 
+app.get('/pokemon', (_req, res) => {
+  res.status(200).json({ port });
+})
+
 app.listen(port, () => {
   console.log(`Aplicação rodando na porta: ${port}`);
 })
