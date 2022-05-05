91d60dfcc55051749a67d7267a301334c7965150
Bruno Matumoto <brunoxd.matumoto@gmail.com>
2022-05-05 16:12:06 -0300

    setup initial project

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.status(200).send(`Project Pokerizado rodando na porta: ${port}`);
+})
+
+app.listen(port, () => {
+  console.log(`Aplicação rodando na porta: ${port}`);
+})
